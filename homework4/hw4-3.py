aab2d456c52a3ccaa2c8c5667847e4155df01050
DanilMargaryan <margaryan.danil@mail.ru>
2024-05-13 23:48:41 +0300

    fix hw4-3

--- homework4/hw4-3.py
+++ homework4/hw4-3.py
@@ -1,47 +1,79 @@
 import multiprocessing
+import threading
 import time
 from codecs import encode
+from datetime import datetime
+import os
 
 
-def worker_a(input_queue, output_pipe):
+def log_message(log_file, message):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    log_file.write(f"{timestamp} - {message}\n")
+    log_file.flush()
+
+
+def process_a(input_queue, output_queue):
     while True:
-        if not input_queue.empty():
-            msg = input_queue.get().lower()
-            output_pipe.send(msg)
-            time.sleep(5)
+        message, send_time = input_queue.get()
+        if message == "exit":
+            output_queue.put(("exit", send_time))
+            break
+        output_queue.put((message.lower(), send_time))
+        time.sleep(5)
 
 
-def worker_b(input_pipe, output_pipe):
+def process_b(input_queue, conn):
     while True:
-        if input_pipe.poll():
-            msg = input_pipe.recv()
-            encoded_msg = encode(msg, 'rot_13')
-            output_pipe.send(encoded_msg)
+        message, send_time = input_queue.get()
+        if message == "exit":
+            conn.send(("exit", send_time, datetime.now()))
+            break
+        encoded_message = encode(message, 'rot_13')
+        conn.send((encoded_message, send_time, datetime.now()))
+
+
+def input_thread(input_queue, log_file):
+    while True:
+        message = input("Введите сообщение 'exit' чтобы выйти из программы: ")
+        send_time = datetime.now()
+        log_message(log_file, f"Отправлено: {send_time.strftime('%Y-%m-%d %H:%M:%S')} - Сообщение: {message}")
+        input_queue.put((message, send_time))
+        if message == "exit":
+            break
+
+
+def output_thread(conn, log_file):
+    while True:
+        encoded_message, send_time, recv_time = conn.recv()
+        if encoded_message == "exit":
+            break
+        send_time_str = send_time.strftime('%Y-%m-%d %H:%M:%S')
+        recv_time_str = recv_time.strftime('%Y-%m-%d %H:%M:%S')
+        log_message(log_file, f"Получено: {recv_time_str} - Закодированное сообщение: {encoded_message}")
+        print(f"{recv_time_str} - Закодированное сообщение: {encoded_message}")
 
 
 if __name__ == "__main__":
-    queue = multiprocessing.Queue()
-    a_to_b, b_to_a = multiprocessing.Pipe()
-    main_to_b, b_to_main = multiprocessing.Pipe()
+    queue_a_b = multiprocessing.Queue()
+    queue_main_a = multiprocessing.Queue()
+    parent_conn, child_conn = multiprocessing.Pipe()
 
-    process_a = multiprocessing.Process(target=worker_a, args=(queue, a_to_b))
-    process_b = multiprocessing.Process(target=worker_b, args=(b_to_a, b_to_main))
-    process_a.start()
-    process_b.start()
+    process_a_proc = multiprocessing.Process(target=process_a, args=(queue_main_a, queue_a_b))
+    process_b_proc = multiprocessing.Process(target=process_b, args=(queue_a_b, child_conn))
 
-    try:
-        with open("artifacts/4_3.txt", "w") as log_file:
-            while True:
-                msg = input("Введите сообщение (или 'exit' для выхода): ")
-                if msg == 'exit':
-                    break
-                timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                log_file.write(f"Отправлено в {timestamp}: {msg}\n")
-                queue.put(msg)
-                if main_to_b.poll(10):  # Ожидание сообщения в течение 10 секунд
-                    received_msg = main_to_b.recv()
-                    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                    log_file.write(f"Получено в {timestamp}: {received_msg}\n")
-    finally:
-        process_a.terminate()
-        process_b.terminate()
+    process_a_proc.start()
+    process_b_proc.start()
+
+    log_file_path = os.path.join("artifacts", "4_3.txt")
+    with open(log_file_path, "a") as log_file:
+        input_thread_proc = threading.Thread(target=input_thread, args=(queue_main_a, log_file))
+        output_thread_proc = threading.Thread(target=output_thread, args=(parent_conn, log_file))
+
+        input_thread_proc.start()
+        output_thread_proc.start()
+
+        input_thread_proc.join()
+        output_thread_proc.join()
+
+    process_a_proc.join()
+    process_b_proc.join()
